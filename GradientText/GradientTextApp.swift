981d4ef51b0e9fe51864c71344b09f0fbd196c90
githubuiux <uiuxista@gmail.com>
2023-06-27 21:15:57 +0300

    Initial Commit

---
+++ GradientText/GradientTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientTextApp.swift
+//  GradientText
+//
+//  Created by Levent on 27.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct GradientTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
